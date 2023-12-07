38e5f68486082ca2df5595793c5b879b211bb025
KujtimGj <kujtim.gjokaj@harrisia.com>
2023-12-07 19:36:45 +0100

    get user by id

--- controller/userController.js
+++ controller/userController.js
@@ -82,9 +82,20 @@ const getAllUsers = async(req,res)=>{
   }
 }
 
+const getSingleUser = async(req,res)=>{
+  try{
+    const {id} = req.params;
+    const user = await User.findById({id});
+    res.status(200).json(user)
+  }catch(error){
+    res.status(400).json({error:error.message})
+  }
+}
+
 module.exports = {
   loginUser,
   signupUser,
   editUser,
-  getAllUsers
+  getAllUsers,
+  getSingleUser
 };
